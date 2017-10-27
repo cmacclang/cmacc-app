ce578c650c729f5ae10e86ceec6307c15c26428a
Willem Veelenturf <wveelenturf@bol.com>
2017-10-27 15:31:03 +0200

    Add forms

--- server.js
+++ server.js
@@ -27,11 +27,14 @@ app.use(expressSession({
 app.use('/assets', express.static('assets'));
 app.use('/dist', express.static('dist'));
 
+app.use('/lib/formiojs', express.static('node_modules/formiojs/dist'));
+
 app.use(require('./routes/testRoute'));
 
 app.use(require('./routes/authRoute'));
 app.use(require('./routes/apiRoute'));
 app.use(require('./routes/pdfRoute'));
+app.use(require('./routes/formioRoute'));
 app.use(require('./routes/loaderRoute'));
 app.use(require('./routes/reposRoute'));
 app.use(require('./routes/contractRoute'));
@@ -40,7 +43,6 @@ app.use(require('./routes/gistsRoute'));
 app.use(require('./routes/editorRoute'));
 app.use(require('./routes/cmaccRoute'));
 app.use(require('./routes/indexRoute'));
-app.use(require('./routes/formioRoute'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
